53d232a96c23142e4c08ee19246ccd69e46cca23
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-10-29 22:40:04 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="fixed-bottom" >
             <div className="p-1 bg-white font-weight-bold text-primary">
                 <span>Санкт-Петербург 2020 </span>
-                <a href="https://github.com/Zakycka163/RUPD" target="_blank" title="Репозиторий"
+                <a href="https://github.com/Zakycka163/family_budget" target="_blank" title="Репозиторий"
                    rel="noreferrer noopener">
                     <img src="/img/ico.ico" height="35px" alt="Иконка" />
                 </a>
