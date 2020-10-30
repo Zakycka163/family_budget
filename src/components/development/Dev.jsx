09936087284bf696d586c2132e5b74eb20f33784
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-10-30 21:42:29 +0300

    Update Dev.jsx

--- src/components/development/Dev.jsx
+++ src/components/development/Dev.jsx
@@ -5,7 +5,12 @@ const Dev = (props) => {
     return (
         <div>
             <PageTitle title={props.title} />
-            test
+            <div className="px-3 py-2 bg-success text-white page-title">
+                <div className="h5" id="page_title">Test</div>
+            </div>
+            <div className="px-3 py-4 bg-light">
+                ПРВЫЛОРРМ
+            </div>
         </div>
     )
 }
