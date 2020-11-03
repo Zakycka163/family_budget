03369b2e8d2354b7f0e781affe7df01e00fba136
Dmitry St <dmitry.st@msp360.com>
2020-11-03 19:14:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 import App from './App';
-import store from './redux/store'
+import store from './redux/redux-store'
 
 const rerenderEntireTree = () => {
     ReactDOM.render(
