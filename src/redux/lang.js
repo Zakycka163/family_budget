9aa27d3c0090990d0fea43b790e26a5bfab49bf7
Dmitry St <dmitry.st@msp360.com>
2020-11-03 19:13:56 +0300

    Update lang.js

---
+++ src/redux/lang.js
@@ -0,0 +1,30 @@
+const lang = {
+    en:{
+        menu: {
+            main: {
+                item: "Main",
+                title: "Main page"
+            },
+            dev: "Development",
+            about: "About",
+            lang: "en",
+            login: "Login"
+        },
+        footer: "Saint Petersburg"
+    },
+    ru:{
+        menu: {
+            main: {
+                "item": "Главная",
+                "title": "Главная страница"
+            },
+            dev: "Разработка",
+            about: "Инфо",
+            lang: "ru",
+            login: "Вход"
+        },
+        footer: "Санкт-Петербург"
+    }
+}
+
+export default lang;
