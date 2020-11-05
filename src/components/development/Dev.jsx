36055bf9d5663f3fab25958e06148a439d7deb21
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-11-05 23:13:08 +0300

    Update Dev.jsx

--- src/components/development/Dev.jsx
+++ src/components/development/Dev.jsx
@@ -15,24 +15,24 @@ const tableBody = (table) => {
 }
 
 const Dev = (props) => {
-    let GTable = () => {
+    /*let GTable = () => {
         props.dispatch({type:'GET-TABLE'});
-    }
+    }*/
     return (
         <div>
             <PageTitle title={props.title} />
 
             <div className="px-3 py-2 bg-success text-white page-title">
-                <div className="btn-group btn-group-sm">
+                {/*<div className="btn-group btn-group-sm">
                     <button type="button" className="btn btn-primary mr-md-1" onClick={ GTable }>Получить данные о файле</button>
                     <button className="btn btn-primary mr-md-1">Получить листа</button>
-                </div>
-                <div className="h5">{props.state.name}</div>
+                </div>*/}
+                <div className="h5">{props.title}</div>
             </div>
             <div className="px-3 py-4 bg-light">
                 <table className="table table-bordered table-sm">
                     <tbody>
-                        {tableBody(props.state.table)}
+                        {tableBody([])}
                     </tbody>
                 </table>
             </div>
