f80f9f7873843d26d4416807949e0633e61ab775
Dmitry St <dmitry.st@msp360.com>
2020-11-06 10:22:42 +0300

    add margin

--- src/components/navBar/Lang.jsx
+++ src/components/navBar/Lang.jsx
@@ -9,8 +9,8 @@ const Lang = (props) => {
     }
     return (
         <div className="mr-md-2">
-            <img src={"/img/"+props.lang.en+".png"} type="button" height="30px" alt="en" onClick={onLangChange}/>
-            <img src={"/img/"+props.lang.ru+".png"} type="button" height="30px" alt="ru" onClick={onLangChange} /> 
+            <img className="mr-md-1" src={"/img/"+props.lang.en+".png"} type="button" height="30px" alt="en" onClick={onLangChange}/>
+            <img className="mr-md-1" src={"/img/"+props.lang.ru+".png"} type="button" height="30px" alt="ru" onClick={onLangChange} />
         </div>
     
     )
