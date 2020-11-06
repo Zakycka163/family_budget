a40b771958c4d397decdd966562dccc38ff7c03c
Dmitry St <dmitry.st@msp360.com>
2020-11-06 10:16:09 +0300

    Update dev-reducer.js

--- src/redux/dev-reducer.js
+++ src/redux/dev-reducer.js
@@ -1,5 +1,15 @@
-const devReducer = (state, action) =>{
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+const LANG_CHANGE = 'LANG_CHANGE';
+
+const initialState = '';
+
+const devReducer = (state=initialState, action) =>{
 
 
     return state;
 }
+
+export const changeLangCreator = (value) => ({type: LANG_CHANGE, value: value})
+
+export default devReducer;
