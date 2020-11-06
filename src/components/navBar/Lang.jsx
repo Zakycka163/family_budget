25d27db67daeb8a5a055af086349c7d36d8540bf
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-11-06 08:27:10 +0300

    Lang switcher is updated

--- src/components/navBar/Lang.jsx
+++ src/components/navBar/Lang.jsx
@@ -4,19 +4,15 @@ import {changeLangCreator} from "../../redux/lang-reducer";
 
 const Lang = (props) => {
     let onLangChange = (e) => {
-        let value = e.target.value;
+        let value = e.target.alt;
         props.dispatch(changeLangCreator(value));
     }
     return (
-        <div>
-            <span className="mr-md-1">
-                <img src={"/img/"+props.lang+"_flag.png"} height="35px" alt="flag" />
-            </span>
-            <select className="custom-select mr-md-3" value={props.lang} onChange={onLangChange}>
-                <option value="en">en</option>
-                <option value="ru">ru</option>
-            </select>
+        <div className="mr-md-2">
+            <img src={"/img/"+props.lang.en+".png"} type="button" height="30px" alt="en" onClick={onLangChange}/>
+            <img src={"/img/"+props.lang.ru+".png"} type="button" height="30px" alt="ru" onClick={onLangChange} /> 
         </div>
+    
     )
 }
 
