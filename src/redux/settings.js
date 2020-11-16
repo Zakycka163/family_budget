7d470efcd857178d768e84b73efb63167d95f5ff
Dmitry St <dmitry.st@msp360.com>
2020-11-16 16:38:54 +0300

    Update settings.js

--- src/redux/settings.js
+++ src/redux/settings.js
@@ -5,17 +5,21 @@ const settings = {
         client_email: creads.client_email,
         private_key: creads.private_key,
         docId: '1k_Gg8R0Z6W_UUIqNA_oELWUZZN5hbRU8QQEJTmtigzQ'
-    },
-    accounts: [
-        {
-            login: "Dmitry",
-            password: "123"
-        },
-        {
-            login: "Maria",
-            password: "123"
-        }
-    ]
+    }
 }
+export let accounts = [
+    {
+        login: "test",
+        password: "123"
+    },
+    {
+        login: "Dmitry",
+        password: "123"
+    },
+    {
+        login: "Maria",
+        password: "123"
+    }
+]
 
 export default settings;
