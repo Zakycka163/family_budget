60528beb7df42f713f799c84856607aed64f0b95
Dmitry St <dmitry.st@msp360.com>
2020-11-17 11:30:18 +0300

    DevAPIContainer included to DevContainer and destruct

--- src/components/pages/development/DevContainer.jsx
+++ src/components/pages/development/DevContainer.jsx
@@ -1,7 +1,30 @@
-import DevAPIContainer from "./DevAPIContainer";
-import {connect} from "react-redux";
+import React from "react";
+import Dev from "./Dev";
+import settings from "../../../redux/settings";
+import {GoogleSpreadsheet} from "google-spreadsheet";
 import {setDocProperty} from "../../../redux/dev-reducer";
+import {connect} from "react-redux";
 
+class DevContainer extends React.Component {
+    componentDidMount() {
+        this.auth()
+            .then(
+                (r) => this.props.setDocProperty(r)
+            );
+    }
+    async auth() {
+        const doc = new GoogleSpreadsheet(settings.service.docId);
+        await doc.useServiceAccountAuth({
+            client_email: settings.service.client_email,
+            private_key: settings.service.private_key,
+        });
+        await doc.loadInfo();
+        return {title: doc.title, sheetCount: doc.sheetCount};
+    }
+    render() {
+        return (<Dev page={this.props.page} doc={this.props.doc}/>)
+    }
+}
 
 const mstp = (state) => {
     return {
@@ -11,6 +34,4 @@ const mstp = (state) => {
 }
 const mdtp = {setDocProperty}
 
-const DevContainer = connect(mstp, mdtp) (DevAPIContainer);
-
-export default DevContainer;
+export default connect(mstp, mdtp) (DevContainer);
