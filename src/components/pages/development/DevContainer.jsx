1aaf0878e3c8cb5fc281afb94c0cfdf57770d421
Dmitry St <dmitry.st@msp360.com>
2020-11-19 17:10:48 +0300

    getDocThunk is added

--- src/components/pages/development/DevContainer.jsx
+++ src/components/pages/development/DevContainer.jsx
@@ -1,45 +1,19 @@
 import React from "react";
 import Dev from "./Dev";
-import settings from "../../../redux/settings";
-import {GoogleSpreadsheet} from "google-spreadsheet";
-import {setDocProperty} from "../../../redux/dev-reducer";
+import {getDocThunk} from "../../../redux/dev-reducer";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
 
 class DevContainer extends React.Component {
     componentDidMount() {
-        if (this.props.is_active) {
-            this.auth(this.props.doc)
-                .then(
-                    (r) => this.props.setDocProperty(r)
-                );
-        }
-    }
-    async auth(doc) {
-        let spreadsheet;
-        if (!doc.GoogleSpreadsheet){
-            spreadsheet = new GoogleSpreadsheet(settings.service.docId);
-            await spreadsheet.useServiceAccountAuth({
-                client_email: settings.service.client_email,
-                private_key: settings.service.private_key,
-            });
-        } else{
-            spreadsheet = doc.GoogleSpreadsheet;
-        }
-        await spreadsheet.loadInfo();
-        let expiry_date = new Date(spreadsheet.jwtClient.credentials.expiry_date);
-        return {
-            title: spreadsheet.title,
-            sheetCount: spreadsheet.sheetCount,
-            GoogleSpreadsheet: spreadsheet,
-            expiry_date: expiry_date
-        };
+        this.props.getDocThunk(this.props.is_active, this.props.doc);
     }
     render() {
-        return (this.props.is_active
-                    ? <Dev page={this.props.page} doc={this.props.doc}/>
-                    : <Redirect to="/" />
-            )
+        return (
+            this.props.is_active
+                ? <Dev page={this.props.page} doc={this.props.doc}/>
+                : <Redirect to="/" />
+        )
     }
 }
 
@@ -50,6 +24,6 @@ const mstp = (state) => {
         is_active: state.profile.is_active
     }
 }
-const mdtp = {setDocProperty}
+const mdtp = {getDocThunk}
 
 export default connect(mstp, mdtp) (DevContainer);
--- src/redux/dev-reducer.js
+++ src/redux/dev-reducer.js
@@ -1,3 +1,6 @@
+import {GoogleSpreadsheet} from "google-spreadsheet";
+import settings from "./settings";
+
 const SET_DOC_PROP = 'SET_DOC_PROP';
 const SET_SHEET_PROP = 'SET_SHEET_PROP';
 
@@ -25,7 +28,37 @@ const devReducer = (state = initialState, action) => {
     }
 }
 
-export const setDocProperty = (property) => ({type: SET_DOC_PROP, property});
+const setDocProperty = (property) => ({type: SET_DOC_PROP, property});
 export const setSheetProperty = (index, property) => ({type: SET_SHEET_PROP, index, property})
+
+export const getDocThunk = (is_active, doc) => {
+    return (dispatch) => {
+        if (is_active) {
+            auth(doc)
+                .then((r) => dispatch(setDocProperty(r)));
+        }
+    }
+}
+
+const auth = async (doc) => {
+    let spreadsheet;
+    if (!doc.GoogleSpreadsheet) {
+        spreadsheet = new GoogleSpreadsheet(settings.service.docId);
+        await spreadsheet.useServiceAccountAuth({
+            client_email: settings.service.client_email,
+            private_key: settings.service.private_key,
+        });
+    } else {
+        spreadsheet = doc.GoogleSpreadsheet;
+    }
+    await spreadsheet.loadInfo();
+    let expiry_date = new Date(spreadsheet.jwtClient.credentials.expiry_date);
+    return {
+        title: spreadsheet.title,
+        sheetCount: spreadsheet.sheetCount,
+        GoogleSpreadsheet: spreadsheet,
+        expiry_date: expiry_date
+    };
+}
 
 export default devReducer;
