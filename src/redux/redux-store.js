929b0f86e8b57422f34d62b6e30a6375751a35dd
Dmitry St <dmitry.st@msp360.com>
2020-11-25 19:37:16 +0300

    formReducer is added

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,11 +3,13 @@ import langReducer from "./lang-reducer";
 import devReducer from "./dev-reducer";
 import profileReducer from "./profile-reducer";
 import thunkMiddleware from "redux-thunk"
+import {reducer as formReducer} from "redux-form"
 
 let reducers = combineReducers({
     elements: langReducer,
     googleDoc: devReducer,
-    profile: profileReducer
+    profile: profileReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
